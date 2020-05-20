ffc68104e503d294a20ba734f6ca7ac6d15eee5b
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-20 15:18:18 +0530

    VowelConsonantSwitchCase.c
    C Program  Vowel & Consonant Using SwitchCase

---
+++ VowelConsonantSwitchCase.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("enter the alphabet");
+	scanf("%c",&ch);
+	switch (ch)
+	{
+	
+	case 'A' :
+	case 'a' :
+	case 'E' :
+	case 'e':
+	case 'I':
+	case 'i':
+	case 'O':
+	case 'o':
+	case 'U':
+	case 'u':	
+		printf("%c is a vowel",ch);
+		break;
+		
+		default:
+			printf("%c is not a vowel",ch);
+}
+}
