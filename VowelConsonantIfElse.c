e0f685e399f55f8c5b3ee2e31373231fd52046c9
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-20 15:15:52 +0530

    VowelConsonantIfElse.c
    C program of Vowel & Consonant Using If Else condition .

---
+++ VowelConsonantIfElse.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+
+ int main()
+{
+	char ch;
+	
+
+	printf("enter the alphabet:=");
+	scanf("%c",&ch);
+	
+	  if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U')	{
+		printf("%c is a vowel",ch);
+	}
+	else
+	{
+		printf("%c is s consonant",ch);
+	}
+}
+
