9e471140d2bd1b52ecfecf376d86f13fc0f225e7
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-20 12:45:02 +0530

    even-odd.c
    It is a simple even/odd program in c language

---
+++ even-odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the  number");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		printf("it ia a even number");
+	}
+	else
+	{
+		printf("it is a odd number");
+	}
