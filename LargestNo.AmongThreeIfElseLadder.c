8748d88ec7043e35032f6346fecb768cea3505f1
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-20 15:09:39 +0530

    LargestNo.AmongThreeIfElseLadder.c
    C Program to Find the Largest Number Among Three Numbers using if else ladder .

---
+++ LargestNo.AmongThreeIfElseLadder.c
@@ -0,0 +1,21 @@
+ #include<stdio.h>
+int main()
+{
+	int n1,n2,n3;
+	printf("enter the three number");
+	scanf("%d %d %d",&n1 ,&n2,&n3);
+	if(n1>=n2 && n1>=n3)
+	{
+		printf("%d is the largest number",n1);
+	}
+else if(n2>=n1 && n2>=n3)
+{
+		printf("%d is the largest number",n2);
+	
+}
+else
+{
+		printf("%d is the largest number",n3);
+	
+ }  
+ }
