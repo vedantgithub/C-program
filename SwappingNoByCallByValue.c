2316f66df5ebf26db9f0183df2c5838269e66c31
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-31 20:44:26 +0530

    SwappingNoByCallByValue.c
    swapping two no. by using call value

---
+++ SwappingNoByCallByValue.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+	void swap(int,int); 
+	int main()
+	{
+		int a,b;
+		printf("enter the two no.");
+		scanf("%d%d",&a,&b);
+		swap(a,b);  
+	}
+	void swap(int p,int q)
+	{
+		int r;
+		r=p;
+		p=q;
+		q=r;
+		printf("after swapping");
+		printf("\n value1 =%d,value2=%d",p,q);
+	}
